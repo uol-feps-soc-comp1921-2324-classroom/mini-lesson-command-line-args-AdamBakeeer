af324e2f394d4e98a60d004bedfee6ade2613548
adam bakeer <146668872+AdamBakeeer@users.noreply.github.com>
2024-02-11 19:01:25 +0000

    hi

--- tasks/task.c
+++ tasks/task.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    int num1, num2;
+int main(int argc ,char *argv[]) {
+    //int num1, num2;
 
     // Prompt the user for input
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
+    //printf("Enter the first number: ");
+    //scanf("%d", &num1);
 
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
+    //printf("Enter the second number: ");
+    //scanf("%d", &num2);
+    int num1 = atoi(argv[1]);
 
-    // Calculate and print the sum
+    int num2 = atoi(argv[2]);
+
     int sum = num1 + num2;
-    printf("Sum: %d\n", sum);
+
+    printf("sum of the 2 numbers is %d\n" , sum);
+
+    
 
     return 0; // Exit successfully
 }
